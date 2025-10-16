c37a0d371bc7d169ef56bb2deb2c1deae8a22670
yunjin25-hue <yunjin25@sookmyung.ac.kr>
2025-10-16 15:39:26 +0900

    3

--- main.c
+++ main.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+  //실습 3
+  
+int inc(int counter);
+
 int main(int argc, char *argv[])
 {
-  //실습 2
+  int i=10;
   
-  int i;
+  printf("함수 호출전 i=%d\n",i);
+  i=inc(i); //inc(i)를 i=inc(i)로 변경해야 함수의 반환값(counter값)을 받을 수 있음  
+  printf("함수 호출후 i=%d\n",i);
   
-  for (i=0; i<5; i++) //5번 반복  
-  {
-      static int temp=1; //지역 변수는 중괄호 닫히고 소멸  
-      printf("temp=%d\n",temp);
-      temp++; 
-      /*temp=1만 5번 찍힘  
-      1찍고 변수 소멸 후 새로운 변수 생성해서 다시 1찍고 소멸  
-      */ 
-  }
-
   system("PAUSE");	
   return 0;
 }
+int inc(int counter)
+{
+  counter++;
+  system("PAUSE");	
+  return counter;
+ } 
+  
 
-/* 해결방법 1. static을 지역 변수에 넣으면 소멸되지 않고 끝까지 감  
-            2. int temp=1;을 바깥에 써주기 (for문 안에 있던 건 아예 삭제)  
-*/
