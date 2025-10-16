2f59635ab29aac6461a12a88fdc4d92ea90860ed
yunjin25-hue <yunjin25@sookmyung.ac.kr>
2025-10-16 15:29:01 +0900

    2

--- main.c
+++ main.c
@@ -3,26 +3,24 @@
 
 int main(int argc, char *argv[])
 {
-  //실습 1 
+  //실습 2
   
-  int flag=1;
-  int y;
-  while(flag != 0)
+  int i;
+  
+  for (i=0; i<5; i++) //5번 반복  
   {
-    int y;
-    y=3;
-    flag=0;
+      static int temp=1; //지역 변수는 중괄호 닫히고 소멸  
+      printf("temp=%d\n",temp);
+      temp++; 
+      /*temp=1만 5번 찍힘  
+      1찍고 변수 소멸 후 새로운 변수 생성해서 다시 1찍고 소멸  
+      */ 
   }
-  y=4; 
-  
+
   system("PAUSE");	
   return 0;
 }
 
-/*실행 안되는게 정상 
-:  15번째 줄이 문제라고 뜸(그 줄이 문제거나 그 위가 문제인 것) 
---> y가 선언되지 않음 이라고 뜸  
---> while 안에 선언 되어 있는데?
-y에 집중해서 문제 해결하기
-이유: y가 while 내에서만 해당 --> while 바깥은 선언되지 않은 것 
---> 바깥에도 선언해줘야 함 
+/* 해결방법 1. static을 지역 변수에 넣으면 소멸되지 않고 끝까지 감  
+            2. int temp=1;을 바깥에 써주기 (for문 안에 있던 건 아예 삭제)  
+*/
