933247106ee01fe80af4f330407906deb13af132
yunjin25-hue <yunjin25@sookmyung.ac.kr>
2025-10-16 16:01:28 +0900

    5

--- main.c
+++ main.c
@@ -1,34 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-  //실습 4
+  //실습 5
   
-//전역변수는 알아서 0으로 초기화됨 
-//추가로 초기화 문법 넣으면 원하는 초기화값 들어감  
+void sub(void);
 
-void f(void);
-
-//int i; //이거 없애고 for문마다 int i추가  
 int main(void)
 {
-  int i=0;
-  for(i=0; i<5; i++) //i가 전역 변수라서 여기서도 ++ , 아래에서도 ++ 
-  //따라서 i를 지역변수로 바꾸기  
-  {
-    f();
-  } 
-  system("PAUSE");
-  return 0;
+    int i;
+    for(i=0; i<3; i++)
+     sub();
+    system("PAUSE");
+    return 0;
 }
 
-void f(void)
+void sub(void)
 {
-  int i=0; 
-  for(i=0; i<10; i++)
-    printf("#"); 
-  
-  //여기다가 시스템 퍼즈 넣으면 안됨  
-	
- } 
-  
+     int auto_count=0;
+     static int static_count=0; //정적 할당: 변수 소멸 안 되고 계속 유지  
+     // 따라서 지난 값에 ++ 
+     auto_count++;
+     static_count++;
+     printf("auto_count=%d\n", auto_count);
+     printf("static_count=%d\n",static_count);
+}
 
